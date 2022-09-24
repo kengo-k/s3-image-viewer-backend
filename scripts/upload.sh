f3e506da61c53406b7acdfe07b796fc8fc2e8b39
kengo-k <kurobanekengo@gmail.com>
2022-09-24 20:54:30 +0900

    upload.sh: ディレクトリパス等の見直し

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 #
-# /opt/dataディレクトリ内のファイルを
+# /app/dataディレクトリ内のファイルを
 # 設定ファイルbucket-config.rcで指定したbukectにsyncする
 #
-# ・引数を指定しない場合は/opt/data全体をbucket直下に
-# ・引数を指定した場合は/opt/data内の指定したサブディレクトリをbucketの同名ディレクトリに
+# ・引数を指定しない場合は/apt/data全体をbucket直下に
+# ・引数を指定した場合は/apt/data内の指定したサブディレクトリをbucketの同名ディレクトリに
 # 配置する
+
 source bucket-config.rc
+echo ">upload s3..."
+echo "BUCKET_NAME=$BUCKET_NAME"
+echo "TARGET_DIR=$1"
+
 TARGET_DIR=$1
-aws s3 sync --exact-timestamps --delete --exclude ".DS_Store" /opt/data/$TARGET_DIR s3://$BUCKET_NAME/$TARGET_DIR
+aws s3 sync --exact-timestamps --delete --exclude ".DS_Store" /app/data/$TARGET_DIR s3://$BUCKET_NAME/$TARGET_DIR
