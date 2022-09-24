1944b3400d62b58a337fa49194ba3c46beee940c
kengo-k <kurobanekengo@gmail.com>
2022-09-24 21:33:26 +0900

    アップロードファイルを個別タスク別に分ける

---
+++ bin/tax/upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source bucket-config.rc
+
+docker run \
+  -it \
+  --rm \
+  -v ${HOME}/Documents/tax:/app/data \
+  -v ${PWD}/../../settings:/root/.aws \
+  tool-s3-sync:v1.0.0 \
+  upload.sh $BUCKET_NAME $1
