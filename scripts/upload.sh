fec88417947e91e4e75fdbaf6adef01716ff6a91
kengo-k <kurobanekengo@gmail.com>
2022-09-24 21:34:22 +0900

    upload.sh: 汎用化。バケット名と対象ディレクトリを引数で受け取るだけ

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,10 +7,10 @@
 # ・引数を指定した場合は/apt/data内の指定したサブディレクトリをbucketの同名ディレクトリに
 # 配置する
 
-source bucket-config.rc
 echo ">upload s3..."
-echo "BUCKET_NAME=$BUCKET_NAME"
-echo "TARGET_DIR=$1"
+echo ">BUCKET_NAME=$1"
+echo ">TARGET_DIR=$2"
 
-TARGET_DIR=$1
+BUCKET_NAME=$1
+TARGET_DIR=$2
 aws s3 sync --exact-timestamps --delete --exclude ".DS_Store" /app/data/$TARGET_DIR s3://$BUCKET_NAME/$TARGET_DIR
