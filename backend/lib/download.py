cd5cc28210d32942b3d0ab7721f2030f4ca9e9fe
kengo-k <kurobanekengo@gmail.com>
2022-11-06 13:41:27 +0900

    download.py: sync実装に向けてファイル一覧を取得する

--- backend/lib/download.py
+++ backend/lib/download.py
@@ -10,13 +10,18 @@ load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 client = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
 )
-response = client.list_buckets()
-buckets = response["Buckets"]
 
-print(buckets)
+objects = client.list_objects_v2(Bucket=BUCKET_NAME, Prefix="R4-2022")
+file_info_list = [
+    {"key": content["Key"], "last_modified": content["LastModified"]}
+    for content in objects["Contents"]
+]
+for info in file_info_list:
+    print(info["key"], info["last_modified"])
